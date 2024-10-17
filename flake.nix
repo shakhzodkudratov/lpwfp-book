0ab220862fbb09eb33f420409f146bb819480c06
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2024-10-17 17:51:01 +0500

    init

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { localSystem = { inherit system; }; };
+    in
+    {
+      devShells.default = pkgs.mkShell ({
+        buildInputs = with pkgs; [
+	  haskell.compiler.ghc96
+          zlib
+        ];
+      });
+    }
+  );
+}
